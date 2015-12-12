8530ea82ffe38ffe6095a7b7625a42bdd8217572
Arthur Maltson <arthur@maltson.com>
2015-12-12 16:20:47 -0500

    Get tasks to render as JSON

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,6 @@ class TasksController < ActionController::API
   include ROM::Rails::ControllerExtension
 
   def index
-    render locals: { tasks: rom.relation(:tasks) }
+    render json: { tasks: rom.relation(:tasks) }
   end
 end
