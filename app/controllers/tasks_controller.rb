da892bfd01d90d78e873d6e7fc0acc82c649ecf5
Arthur Maltson <arthur@maltson.com>
2015-12-13 22:21:19 -0500

    Add a test of said tasks
    Turns out when we return an empty relation and try to serialize that to
    JSON we get a stackoverflow for some reason. Not sure if that's a bug in
    ROM.
    
    - add simple test for empty tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,6 @@ class TasksController < ActionController::API
   include ROM::Rails::ControllerExtension
 
   def index
-    render json: { tasks: rom.relation(:tasks) }
+    render json: { tasks: rom.relation(:tasks).to_a }
   end
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
+  it 'should render task json' do
+    get :index
 
+    body = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(body['tasks']).to eq([])
+  end
 end
